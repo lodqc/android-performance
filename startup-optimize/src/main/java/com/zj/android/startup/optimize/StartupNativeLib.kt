6723eb2e3f2ae26cc68eafa92d4d4de87d2d24cd
albert.fu <albert.fu@immotor.com>
2023-08-18 11:39:17 +0800

    首次提交

---
+++ startup-optimize/src/main/java/com/zj/android/startup/optimize/StartupNativeLib.kt
@@ -0,0 +1,21 @@
+package com.zj.android.startup.optimize
+
+internal class StartupNativeLib {
+
+    /**
+     * A native method that is implemented by the 'optimize' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    external fun delayGC(): Unit
+
+    external fun bindCore(threadId: Int, core: Int): Unit
+
+    companion object {
+        // Used to load the 'optimize' library on application startup.
+        init {
+            System.loadLibrary("startup-optimize")
+        }
+    }
+}
